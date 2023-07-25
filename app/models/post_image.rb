6af286e46a7ac004b788a1d79192e6e07b64766c
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-07-25 11:39:54 +0000

    [Add] 4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,10 @@
 class PostImage < ApplicationRecord
+
+  #画像投稿に必要な ActiveStorage の設定
   has_one_attached :image
+
+  #1:NのN側にあたるモデル
+  belongs_to :user
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ #1:Nの1側にあたるモデル
+ has_many :post_images, dependent: :destroy
+
 end
