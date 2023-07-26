411530fa37dd938371ab6117030e5cb0b69b207b
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-07-26 05:16:55 +0000

    [Add] 4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,14 @@ class PostImage < ApplicationRecord
 
   #1:NのN側にあたるモデル
   belongs_to :user
-  
+
+  #画像が設定されていない場合
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 
 end
