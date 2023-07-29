5d8474f5ceb0b1dcd46eaf52d4502fdeb8d3da1b
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-07-29 12:26:46 +0000

    [Finish] 4-18

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,10 +1,17 @@
 class PostCommentsController < ApplicationController
+  #コメント投稿機能
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
+  end
+
+  #コメント投稿削除機能
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
