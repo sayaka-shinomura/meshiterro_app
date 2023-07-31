2ada11e121bccaa206c69139dbd35c9086f6b684
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-07-31 01:24:31 +0000

    [Add] 4-24

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,7 +6,7 @@ class PostImagesController < ApplicationController
   #投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
-    @post_image.user_id = current_user.id
+    @post_image.user_id = current
     if @post_image.save
       redirect_to post_images_path
     else
